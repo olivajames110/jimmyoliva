7b52bd6fba34d73db86f9edf6f65a873dd743922
olivajames110 <olivajames110@gmail.com>
2018-08-09 22:02:40 -0400

    Buttons

--- canvasStars.js
+++ canvasStars.js
@@ -74,6 +74,8 @@ var bigDipBtn = document.getElementById("star-btn-big");
 bigDipBtn.addEventListener('click', function() {
   var ranWidth = Math.floor(Math.random() * (innerWidth - 400) + 400)
   var ranHeight = Math.floor(Math.random() * (innerHeight - 400) + 300)
+
+
   var bigDipStars = {
    s1: {
     x:ranWidth - 50,
